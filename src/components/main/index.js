95ee8d4e7a38e49f779fa0861caf6b3ae042773f
Mark Kulishov <>
2023-02-01 22:33:05 +0200

    init: webpak config, basic folders

---
+++ src/components/main/index.js
@@ -0,0 +1,15 @@
+import "./style.css";
+
+class Main {
+    constructor(props) {
+        this.props = props;
+    }
+
+    async render() {
+        const main = document.createElement("main");
+
+        return main;
+    }
+}
+
+export default Main;
