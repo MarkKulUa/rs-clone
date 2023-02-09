333dd5fd44051f57bad219b948029f9f5af687b0
Anton <bedunkevichaa@gmail.com>
2023-02-09 12:40:35 +0300

    feat: add burger menu

--- src/components/simple-header/simple-header.ts
+++ src/components/simple-header/simple-header.ts
@@ -3,9 +3,16 @@ import template from "./template";
 import "./simple-header.css";
 
 class SimpleHeader extends Component {
+  private navItems: Component[] = [];
+
+  private toAboutLink: Component;
+
   constructor(parentNode: HTMLElement) {
     super(parentNode, "div", ["container", "simple-header"]);
     this.appendHTML(template());
+
+    this.toAboutLink = new Component(this.elem, "a", ["nav-item"], "about");
+    console.log(this.toAboutLink);
   }
 }
 
